d2be54b604077e5f68ba4bd28cce9d8b8f5ec47a
CarlosDiaz03 <115045160+CarlosDiaz03@users.noreply.github.com>
2023-04-03 18:47:04 -0500

    Load

--- index.php
+++ index.php
@@ -15,17 +15,17 @@
 
     <header>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="market.php">Market</a>
+            <a class="navbar-brand" href="index.php">Market</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link" href="market.php" class="sr-only">Home</a>
+                        <a class="nav-link" href="index.php" class="sr-only">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="xander.php" class="sr-only">Catálogo</a>
+                        <a class="nav-link" href="catalogo.php" class="sr-only">Catálogo</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#" class="sr-only">Contáctanos</a>
@@ -81,24 +81,32 @@
 
     <div class="container marketing">
 
-        <div class="card-deck mb-3 text-center">
-            <div class="card mb-4 box-shadow">
-                <div class="card-header">
-                    <h4 class="my-0 font-weight-normal">Polo</h4>
+        <div class="row mb-2">
+            <div class="col-md-6">
+                <div class="card flex-md-row mb-4 box-shadow h-md-250">
+                    <div class="card-body d-flex flex-column align-items-start">
+                        <strong class="d-inline-block mb-2 text-success">Lacoste</strong>
+                        <h3 class="mb-0">
+                            <a class="text-dark" href="#">Polo Simple</a>
+                        </h3>
+                        <br>
+                        <p class="card-text mb-auto">Materiales de mejor calidad, resistente duradero y cómodos.</p>
+                    </div>
+                    <img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Thumbnail [200x250]" style="width: 200px; height: 250px;" src="https://falabella.scene7.com/is/image/FalabellaPE/18777612_3?wid=1004&hei=1500&crop=248,0,1004,1500&qlt=70" data-holder-rendered="true">
                 </div>
-                <img src="https://falabella.scene7.com/is/image/FalabellaPE/18777612_1?wid=1004&hei=1500&crop=248,0,1004,1500&qlt=70" alt="" class="d-block w-100 h-100">
             </div>
-            <div class="card mb-4 box-shadow">
-                <div class="card-header">
-                    <h4 class="my-0 font-weight-normal">Polo Camisero</h4>
+            <div class="col-md-6">
+                <div class="card flex-md-row mb-4 box-shadow h-md-250">
+                    <div class="card-body d-flex flex-column align-items-start">
+                        <strong class="d-inline-block mb-2 text-success">Lacoste</strong>
+                        <h3 class="mb-0">
+                            <a class="text-dark" href="#">Polo Piqué</a>
+                        </h3>
+                        <br>
+                        <p class="card-text mb-auto">Polo Piqué de algodón, cálido y cómodo. Para looks cotidianos</p>
+                    </div>
+                    <img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Thumbnail [200x250]" src="https://home.ripley.com.pe/Attachment/WOP_5/2005289426983/2005289426983-1.jpg" data-holder-rendered="true" style="width: 200px; height: 250px;">
                 </div>
-                <img src="https://www.gamarra.com.pe/wp-content/uploads/2022/12/DSC01218-480x600.jpg" alt="" class="d-block w-100 h-100">
-            </div>
-            <div class="card mb-4 box-shadow">
-                <div class="card-header">
-                    <h4 class="my-0 font-weight-normal">Polo Deportivo</h4>
-                </div>
-                <img src="https://images.asos-media.com/products/polo-deportivo-con-diseno-color-block-y-acabado-muy-ligero-para-tenis-de-lacoste/22773713-3?$n_640w$&wid=513&fit=constrain" alt="" class="d-block w-100 h-100">
             </div>
         </div>
 
@@ -109,11 +117,11 @@
 
         <div class="row featurette">
             <div class="col-md-7">
-                <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
-                <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+                <h2 class="featurette-heading">Nueva prenda en mano. <span class="text-muted">Nuevo estilo de vestir.</span></h2>
+                <p class="lead">Versátiles y fáciles de combinar, ya que puede usarlos tanto para ocasiones formales como informales.</p>
             </div>
             <div class="col-md-5">
-                <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" style="width: 500px; height: 500px;" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22500%22%20height%3D%22500%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20500%20500%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_187493f049a%20text%20%7B%20fill%3A%23AAAAAA%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A25pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_187493f049a%22%3E%3Crect%20width%3D%22500%22%20height%3D%22500%22%20fill%3D%22%23EEEEEE%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22185.1171875%22%20y%3D%22261.1%22%3E500x500%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true">
+                <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" style="width: 500px; height: 500px;" src="https://th.bing.com/th/id/R.fc9f1ec9e5961d76f3b9dd2d44a36262?rik=HKc0zuezeOJ8tA&pid=ImgRaw&r=0" data-holder-rendered="true">
             </div>
         </div>
 
@@ -121,15 +129,14 @@
 
         <div class="row featurette">
             <div class="col-md-7 order-md-2">
-                <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
-                <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+                <h2 class="featurette-heading">No llamamos la atención. <span class="text-muted">La elegencia se mira por sí misma.</span></h2>
+                <p class="lead">Hecho con algodón piqué, un tejido suave y transpirable que se adapta a su cuerpo y le brinda frescura y confort.</p>
             </div>
             <div class="col-md-5 order-md-1">
-                <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22500%22%20height%3D%22500%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20500%20500%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_187493f049b%20text%20%7B%20fill%3A%23AAAAAA%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A25pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_187493f049b%22%3E%3Crect%20width%3D%22500%22%20height%3D%22500%22%20fill%3D%22%23EEEEEE%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22185.1171875%22%20y%3D%22261.1%22%3E500x500%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" data-holder-rendered="true" style="width: 500px; height: 500px;">
+                <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="500x500" src="https://th.bing.com/th/id/R.608e7628d2b59c3b9b86bb13055eb86f?rik=DZgLH%2bCpMMB0tQ&riu=http%3a%2f%2fwww.uredisam.com%2fwp-content%2fuploads%2f2020%2f03%2fpolos-lacoste-polo-tenis-lacoste-sport-ultra-light-negro-hombre.jpg&ehk=6k8OlYNFg6LYKMwyVs5mrvwVR0tUAUah8xLkouNtqQc%3d&risl=&pid=ImgRaw&r=0" data-holder-rendered="true" style="width: 500px; height: 500px;">
             </div>
         </div>
 
-        <hr class="featurette-divider">
 
         <!-- /END THE FEATURETTES -->
 
@@ -137,7 +144,7 @@
 
 
 
-
+    <hr>
 
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
@@ -145,11 +152,12 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 <footer class="blog-footer">
-      <center>
+    <center>
         <p>Página hecha por <a href="https://getbootstrap.com/">Bootstrap</a> by <a href="">@MD</a>.</p>
         <p>
             Hecha para información
         </p>
-      </center>
+    </center>
 </footer>
+
 </html>
