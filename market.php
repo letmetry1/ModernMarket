e7fe121b89f0749006538744cda0ea79841101a4
Rafaels69 <leonkenzo@gmail.com>
2023-04-03 17:55:05 -0500

    Update market.php

--- market.php
+++ market.php
@@ -146,7 +146,7 @@
 </body>
 <footer class="blog-footer">
       <center>
-        <p>Página hecha por <a href="https://getbootstrap.com/">Bootstrap</a> by <a href="">@Rafael</a>.</p>
+        <p>Página hecha por <a href="https://getbootstrap.com/">Bootstrap</a> by <a href="">@MD</a>.</p>
         <p>
             Hecha para información
         </p>
