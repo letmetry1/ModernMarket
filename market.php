f191e61b90ec72009a2a3ffd38a43fbfc10b8dd9
CarlosDiaz03 <115045160+CarlosDiaz03@users.noreply.github.com>
2023-04-03 17:24:50 -0500

    Load

--- market.php
+++ market.php
@@ -12,7 +12,7 @@
 <body>
     
 <header>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-success">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-secondary">
     <a class="navbar-brand" href="market.php">Market</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
