3d3e80a330ba4ad163abb89d31ef01638506a273
Rafaels69 <leonkenzo@gmail.com>
2023-04-04 09:08:04 -0500

    Update contactanos.php

--- contactanos.php
+++ contactanos.php
@@ -59,7 +59,62 @@
     </div>
 </section>
 
-<hr>
+
+
+<div class="container">
+		<h1 class="text-center mt-5 mb-5">Acerca de nuestra empresa</h1>
+        
+		<div class="row">
+			<div class="col-md-6">
+				<h2>Nuestra historia</h2>
+				<p>Texto descriptivo sobre la historia de la empresa.</p>
+				<p>Más texto descriptivo sobre la historia de la empresa.</p>
+			</div>
+			<div class="col-md-6">
+				<h2>Nuestra visión</h2>
+				<p>Texto descriptivo sobre la visión de la empresa.</p>
+				<p>Más texto descriptivo sobre la visión de la empresa.</p>
+			</div>
+		</div>
+	</div>
+    <br><br>
+	<section class="container">
+		<div class="row">
+			<div class="col-lg-8 mx-auto">
+				<h2 class="text-center">Guía de compra</h2>
+
+				<div class="bg-light p-3">
+					<h3>Delivery</h3>
+					<p>Ofrecemos delivery en toda la ciudad de X. Los pedidos se entregan en un plazo máximo de 24 horas.</p>
+				</div>
+
+				<div class="bg-light p-3 mt-3">
+					<h3>Pagos</h3>
+					<p>Aceptamos pagos en efectivo y con tarjeta de crédito. En caso de pago con tarjeta, se debe presentar la tarjeta en el momento de la entrega.</p>
+				</div>
+
+				<div class="bg-light p-3 mt-3">
+					<h3>Cómo realizar un pedido</h3>
+					<p>Para realizar un pedido, puede hacerlo a través de nuestro sitio web, llamando a nuestro número de teléfono o visitando nuestra tienda física. Una vez que recibimos su pedido, nos comunicaremos con usted para confirmar la entrega y el método de pago.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+<br>
+
+	<center>
+		<a href="https://www.youtube.com/watch?v=G04SWoEP7BY" target="_blank">
+			<img src="https://w7.pngwing.com/pngs/477/596/png-transparent-computer-icons-facebook-blog-facebook-logo-desktop-wallpaper-facebook-icon.png" width="50px" height="50" alt="Facebook">
+		</a>
+
+		<a href="https://www.youtube.com/watch?v=G04SWoEP7BY" target="_blank">
+			<img src="https://www.freepnglogos.com/uploads/logo-ig-png/logo-ig-instagram-new-logo-vector-download-5.png" width="50px" height="50" alt="Instagram">
+		</a>
+
+		<a href="https://www.youtube.com/watch?v=G04SWoEP7BY" target="_blank">
+			<img src="https://static.vecteezy.com/system/resources/previews/018/930/466/original/twitter-logo-twitter-icon-transparent-free-free-png.png" width="60px" height="70" alt="Twitter">
+		</a>
+	</center>
 
 
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
