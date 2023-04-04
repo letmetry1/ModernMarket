9b1230546cde11686b399705cf0b3a41586227db
CarlosDiaz03 <115045160+CarlosDiaz03@users.noreply.github.com>
2023-04-04 08:52:11 -0500

    Load

--- contactanos.php
+++ contactanos.php
@@ -8,7 +8,7 @@
     <title>Modern Market</title>
     <link rel="shortcut icon" href="Assets/img/logo/icon.png" type="image/x-icon">
 
-    
+
     <link rel="stylesheet" href="Assets/css/style.css" type="text/css">
 
     <!-- Css Styles -->
@@ -26,28 +26,59 @@
 <?php include_once('./View/header.php'); ?>
 
 
+<section class="contact spad">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-6">
+                <div class="contact__text">
+                    <div class="section-title">
+                        <h2>Contáctanos</h2>
+                        <p>Para absolver tus dudas, rellena el formulario y te proporcionaremos la información necesaria.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 col-md-6">
+                <div class="contact__form">
+                    <form action="#">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <input type="text" placeholder="Nombre">
+                            </div>
+                            <div class="col-lg-6">
+                                <input type="text" placeholder="Email">
+                            </div>
+                            <div class="col-lg-12">
+                                <textarea placeholder="Mensaje"></textarea>
+                                <center><button type="submit" class="site-btn">Enviar</button></center>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<hr>
 
 
-    <hr>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
-    <!-- Js Plugins -->
-    <script src="Assets/js/bootstrap.min.js"></script>
-    <script src="Assets/js/jquery-3.3.1.min.js"></script>
-    <script src="Assets/js/jquery.magnific-popup.min.js"></script>
-    <script src="Assets/js/jquery-ui.min.js"></script>
-    <script src="Assets/js/mixitup.min.js"></script>
-    <script src="Assets/js/jquery.countdown.min.js"></script>
-    <script src="Assets/js/jquery.slicknav.js"></script>
-    <script src="Assets/js/owl.carousel.min.js"></script>
-    <script src="Assets/js/jquery.nicescroll.min.js"></script>
-    <script src="Assets/js/main.js"></script>
+<!-- Js Plugins -->
+<script src="Assets/js/bootstrap.min.js"></script>
+<script src="Assets/js/jquery-3.3.1.min.js"></script>
+<script src="Assets/js/jquery.magnific-popup.min.js"></script>
+<script src="Assets/js/jquery-ui.min.js"></script>
+<script src="Assets/js/mixitup.min.js"></script>
+<script src="Assets/js/jquery.countdown.min.js"></script>
+<script src="Assets/js/jquery.slicknav.js"></script>
+<script src="Assets/js/owl.carousel.min.js"></script>
+<script src="Assets/js/jquery.nicescroll.min.js"></script>
+<script src="Assets/js/main.js"></script>
 </body>
 
-<?php include_once('./View/footer.php'); ?>    
+<?php include_once('./View/footer.php'); ?>
 
 </html>
