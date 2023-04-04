eb8560c78467e0859f74324e354cb650d5944afb
letmetry1 <rafaelzamora146@gmail.com>
2023-04-04 07:43:25 -0500

    asd

---
+++ View/footer.php
@@ -0,0 +1,9 @@
+<footer class="blog-footer">
+    <center>
+        <p>Página hecha a través de <a href="https://getbootstrap.com/">Bootstrap</a> por el grupo <strong>Modernizing Peru</strong>.</p>
+        <p>
+            Hecha para información
+        </p>
+        <br>
+    </center>
+</footer>
--- index.php
+++ index.php
@@ -23,51 +23,7 @@
 
 </head>
 
-<body>
-    <!-- Page Preloder -->
-    <div id="preloder">
-        <div class="loader"></div>
-    </div>
-    <!-- Fin Page Preloder -->
-
-    <!-- Offcanvas Menu Begin -->
-    <div class="offcanvas-menu-overlay"></div>
-    <div class="offcanvas-menu-wrapper">
-        <div class="offcanvas__close">+</div>
-        <div class="offcanvas__logo">
-            <a href="index.php"><img src="Assets/img/logo/icon.png" alt=""></a>
-        </div>
-        <div id="mobile-menu-wrap"></div>
-    </div>
-    <!-- Offcanvas Menu End -->
-
-    <!-- Header Section Begin -->
-    <header class="header">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-xl-3 col-lg-2">
-                    <div class="header__logo">
-                        <a href="index.php"><img src="Assets/img/logo/Modernizing.png" alt=""></a>
-                    </div>
-                </div>
-                <div class="col-xl-6 col-lg-7">
-                    <center>
-                        <nav class="header__menu">
-                            <ul>
-                                <li><a href="index.php">Home</a></li>
-                                <li><a href="catalogo.php">Catálogo</a></li>
-                                <li><a href="contactanos.php">Contáctanos</a></li>
-                            </ul>
-                        </nav>
-                    </center>
-                </div>
-            </div>
-            <div class="canvas__open">
-                <i class="fa fa-bars" style="color: #ffffff;"></i>
-            </div>
-        </div>
-    </header>
-    <!-- Header Section End -->
+<?php include_once('./View/header.php'); ?>
 
     <section>
         <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
@@ -199,14 +155,7 @@
     <script src="Assets/js/jquery.nicescroll.min.js"></script>
     <script src="Assets/js/main.js"></script>
 </body>
-<footer class="blog-footer">
-<center>
-        <p>Página hecha a través de <a href="https://getbootstrap.com/">Bootstrap</a> por el grupo <strong>Modernizing Peru</strong>.</p>
-        <p>
-            Hecha para información
-        </p>
-        <br>
-    </center>
-</footer>
+
+<?php include_once('./View/footer.php'); ?>    
 
 </html>
