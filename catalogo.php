cbe12055e90e0394dbc60cc5c01de225cb4a6794
CarlosDiaz03 <115045160+CarlosDiaz03@users.noreply.github.com>
2023-04-04 07:59:31 -0500

    Load

--- catalogo.php
+++ catalogo.php
@@ -8,10 +8,6 @@
     <title>Modern Market</title>
     <link rel="shortcut icon" href="Assets/img/logo/icon.png" type="image/x-icon">
 
-
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-
     <!-- Css Styles -->
     <link rel="stylesheet" href="Assets/css/bootstrap.min.css" type="text/css">
     <link rel="stylesheet" href="Assets/css/font-awesome.min.css" type="text/css">
